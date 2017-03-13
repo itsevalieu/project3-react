f46c8ba5e1daeb073ccb91edd255b4b7e70e1fe7
EternalVigil <theeternalvigil@gmail.com>
2017-03-13 14:51:43 -0700

    Migrated from project3 repo
    created new repo for react implimentation

---
+++ components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+	render() {
+		return(
+			<div className="Header">
+			Placeholder for Header
+			<ul>
+			<li><a href="/about">Test link</a></li>
+			</ul>
+			</div>
+			);
+	}
+}
+
+export default Header;
