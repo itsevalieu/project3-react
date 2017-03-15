06f4ea58bf84de2bc036d0a6ea27f572a749ce3f
Brandon <theeternalvigil@gmail.com>
2017-03-14 21:26:04 -0700

    created new placeholder for FAQ

---
+++ src/containers/FAQ.jsx
@@ -0,0 +1,12 @@
+import React, { Components } from 'react';
+
+class FAQ extends Compnents {
+	render() {
+		return(
+			<div className="faqContainer">
+				HELLO FROM FAQ
+			</div>
+			);
+	}
+}
+export default FAQ;
--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ import './index.css';
 import App from './App';
 import Home from './containers/Home';
 import About from './containers/About';
+import Dashboard from './containers/Dashboard';
+import FAQ from './containers/FAQ';
 
 const appEntry = <Router history={browserHistory}>
 <Route path='/' component={App}>
 			<IndexRoute component={Home}/>
-
 			<Route path='/about' component={About}/>
-
+			<Route path='/dashboard' component={Dashboard}/>
+			<Route path='/faq' component={FAQ}/>
 		</Route>
 </Router>
 ReactDOM.render(
