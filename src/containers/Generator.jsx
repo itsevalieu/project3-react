f9f0f1dd63a2bc9de9f1cb81e4502939c90ddb62
Brandon <theeternalvigil@gmail.com>
2017-03-18 12:29:23 -0700

    modified routes to include generator page, created generator container

---
+++ src/containers/Generator.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+	
+console.log('generator');
+
+class Generator extends Component {
+
+	render(){
+		return(
+			<div id='generator'>
+			</div>
+		);
+	}
+}
+
+export default Generator;
