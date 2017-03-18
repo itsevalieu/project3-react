e703d49f0794c80bd0e8d08b07d8f20293bf8b88
Brandon <theeternalvigil@gmail.com>
2017-03-18 12:38:07 -0700

    created generator.jsx for /generator link

--- src/containers/Generator.jsx
+++ src/containers/Generator.jsx
@@ -6,10 +6,27 @@ class Generator extends Component {
 
 	render(){
 		return(
-			<div id='generator'>
-			</div>
+			<main id='generator'>
+				<div id='index-banner' className='section no-pad-bot'>
+					<div className='container'>
+						<div className='center_section'>
+							<div className='header center red-text'><span>CODE</span><span>SPIRATION</span>
+								<h1 className='header center red-text'><span>Idea Generator</span></h1>
+							</div>
+							<div className='row center'>
+								<h5 className='header col s12 light'>
+									Need some inspiration for your next coding project?<br/>
+									<b>Well, let's get picking!</b>
+								</h5>
+							</div>
+							<div className='row center'>
+								<a href='#' type='submit' name='action' className='btn-large waves-effect waves-light red'>Eenie Meenie Miney Mo!!!</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
 		);
 	}
 }
-
 export default Generator;
