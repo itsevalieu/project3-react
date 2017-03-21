23e5778edc96cf6b743eb2b7437bb5b8a2041d1d
Brandon <theeternalvigil@gmail.com>
2017-03-20 21:33:08 -0700

    rewriting javascript events for modal actions due to jQuery conflicts

--- src/components/signUpModal.jsx
+++ src/components/signUpModal.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 
+function closeSignUpModal() {
+	const closeSignUpModal = document.querySelector('#signUpModal');
+	console.log(closeSignUpModal);
+	closeSignUpModal.style.display = 'none';
+}
+
+function submitUserDetails() {
+	console.log('submit sign up details');
+	/*
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', '/api/user');
+	xhr.onload = function() {
+		if(){}
+			else{}
+	};
+	xhr.send();
+	*/
+} 
+
 const SignUpModal = () => {
-	console.log('sign up');
 	return (
 		<div id='signUpModal' className='modal'>
-			<h4>Sign Up</h4>
+		<div className='modal-content'>
+		<div className='row'>
+			<div className='col s10'>
+				<h4>Sign Up</h4>
+			</div>
+			<div className='col s1'>
+				<button id='closeSignUp' className='btn black modal-close' onClick={closeSignUpModal}>X</button>
+			</div>
+		</div>
 			<form className='col s12'>
 				<div className='row'>
 					<div className='input-field col s6'>
@@ -28,10 +54,11 @@ const SignUpModal = () => {
 				</div>
 				<div className='chips chips-initial'/>
 				 <div className="modal-footer"> 
-				 	<a href="#!" className="modal-action modal-close waves-effect waves-green btn red">Submit</a> 
+				 	<a href="#!" onClick={submitUserDetails} className="modal-action modal-close waves-effect waves-green btn red">Submit</a> 
 				 	<a className="modal-action modal-close waves-effect waves-green btn-flat" href="#signInModal">Already have an Account? Click Here to Sign-in!</a> 
 				 </div>
 			</form>
+			</div>
 		</div>
 		);
 }
