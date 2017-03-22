0cf814001ce3d5fa9fc119bc4f8f75d288cdcb58
Brandon <theeternalvigil@gmail.com>
2017-03-21 20:58:18 -0700

    cleaned up modal code

--- src/components/signUpModal.jsx
+++ src/components/signUpModal.jsx
@@ -18,9 +18,6 @@ const SignUpModal = () => {
 	return (
 		<div>
 			<Row>
-				<Col s={10}>Create an account:</Col>
-			</Row>
-			<Row>
 				<Col s={6}>
 					<Input placeholder='Full Name' s={12} label='Full Name' validate>
 						<Icon>perm_identity</Icon>
@@ -64,48 +61,6 @@ const SignUpModal = () => {
 				</Col>
 			</Row>
 		</div>
-
-		/*
-		<div id='signUpModal' className='modal'>
-		<div className='modal-content'>
-		<div className='row'>
-			<div className='col s10'>
-				<h4>Sign Up</h4>
-			</div>
-			<div className='col s1'>
-				<button id='closeSignUp' className='btn black modal-close' onClick={closeSignUpModal}>X</button>
-			</div>
-		</div>
-			<form className='col s12'>
-				<div className='row'>
-					<div className='input-field col s6'>
-						<input id='name' type='text' className='validate' />
-						<label htmlFor='name'>Name</label>
-					</div>
-					<div className='input-field col s6'>
-						<input id='email' type='email' className='validate' />
-						<label htmlFor='email'>Email</label>
-					</div>
-				</div>
-				<div className='row'>
-					<div className='input-field col s6'>
-						<input id='userName' type='text' className='validate' />
-						<label htmlFor='userName'>User Name</label>
-					</div>
-					<div className='input-field col s6'>
-						<input id='password' type='text' className='validate' />
-						<label htmlFor='password'>Password</label>
-					</div>
-				</div>
-				<div className='chips chips-initial'/>
-				 <div className="modal-footer"> 
-				 	<a href="#!" onClick={submitUserDetails} className="modal-action modal-close waves-effect waves-green btn red">Submit</a> 
-				 	<a className="modal-action modal-close waves-effect waves-green btn-flat" href="#signInModal">Already have an Account? Click Here to Sign-in!</a> 
-				 </div>
-			</form>
-			</div>
-		</div>
-		*/
 		);
 }
 export default SignUpModal;
