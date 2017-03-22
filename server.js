7cf791e78b71507fd8c519aea3b44d4116a58ea7
Kevin Pagtakhan <kevin@floqast.com>
2017-03-21 21:29:13 -0700

    Use webpack

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 
 // Static Directory
 // -------------------------------------------------
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "src")));
 
 // MongoDB Configuration
 // -------------------------------------------------
@@ -126,8 +126,14 @@ app.use("/tech", techRoutes);
 app.use("/user", userRoutes);
 
 // app.use("/", controllers);
-app.use(express.static(path.join(__dirname, 'static')));
-app.get("*", express.static(path.join(__dirname, './public/index.html')));
+// app.use(express.static(path.join(__dirname, 'src')));
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, './src/index.html'));
+});
+
+// app.get('/about', function (req, res) {
+//     console.log('hello');
+// });
 
 // Listener
 // -------------------------------------------------
