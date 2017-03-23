027534ec680ff2e94c7c237d0faf93e48ce15578
Brandon <theeternalvigil@gmail.com>
2017-03-22 21:34:25 -0700

    modified dashboard layout, created new component for user sidebar navigation

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,5 +1,6 @@
 // Main Dashboard - dashboard.jsx
 import React, { Component } from "react";
+import { Row, Button, Input, Icon, Col } from 'react-materialize';
 import DashRightColumn from './DashRightColumn.jsx';
 //import "./dashboard.css";
 
@@ -7,8 +8,15 @@ class dashboard extends Component{
 	render(){
 		return(
 			<div>
-			TEST-DASHBOARD
-			<DashRightColumn />
+				<Row>
+					<Col s={3}>
+						<h1>User Navigation</h1>
+					</Col>
+					<Col s={9}>
+						TEST-DASHBOARD
+						<DashRightColumn />
+					</Col>
+				</Row>
 			</div>
 			);
 	}
