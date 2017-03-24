da751a2abc7d34e5dcde2a32ae6219efddc71d3c
Eva Lieu <itsevalieu@gmail.com>
2017-03-23 19:42:27 -0700

    Import css test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, Router, browserHistory, IndexRoute } from 'react-router';
-//import './index.css';
+import './index.css';
 
 import App from './App';
 import Home from './containers/Home.jsx';
