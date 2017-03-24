4f1ff9a960a6519abd3331d4a0caafacd31a270c
Eva Lieu <itsevalieu@gmail.com>
2017-03-24 07:29:39 -0700

    Trying to fix css loaders in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,16 +18,21 @@ module.exports = {
                 test: /\.jsx?$/,
                 loaders: ['babel-loader'],
                 include: path.join(__dirname, 'src')
-            }
-        ],
-        rules: [
+            },
             {
                 test: /\.css$/,
-                use: [ 
-                    { loader: "style-loader" }, 
-                    { loader: "css-loader" }
-                ],
-                include: path.join(__dirname, 'public')
+                loader: combineLoaders([
+                    {
+                        loader: 'style-loader'
+                    }, 
+                    {
+                        loader: 'css-loader',
+                        query: {
+                            modules: true,
+                            localIdentName: '[name]__[local]___[hash:base64:5]'
+                        }
+                    }
+                ])
             },
             {
                 test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
@@ -39,3 +44,12 @@ module.exports = {
         ]
     }
 };
+
+  //           {
+  //               test: /\.css$/,
+  //               use: [ 
+  //                   { loader: "style-loader" }, 
+  //                   { loader: "css-loader" }
+  //               ],
+  //               include: path.join(__dirname, 'public')
+  //           },
