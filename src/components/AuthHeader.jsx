881b38dd4aa8566ccd021f2a6eb0b2cee55946b5
Brandon <theeternalvigil@gmail.com>
2017-04-11 21:36:33 -0700

    added styling to generator components from design docs

--- src/components/AuthHeader.jsx
+++ src/components/AuthHeader.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Button, Modal } from 'react-materialize';
 import Logo from '../../public/assets/images/logo.png';
-import SignInModal from './signInModal.jsx';
-import SignUpModal from './signUpModal.jsx';
 import '../../public/assets/css/header.css';
 
 class AuthHeader extends Component {
@@ -12,12 +10,14 @@ class AuthHeader extends Component {
 				<nav id='myTopnav' className='topnav'>
 					<div className='container'>
 						<a href='/' className='nav-logo'>
-							<img className='logoImage' src={Logo} />
+							<img className='logoImage' src={Logo} alt='CodeSpiriation Logo' />
 						</a>
 						<a href='/submissions'>Submissions</a>
 						<a href='/generator'>Generator</a>
 						<a href='/dashboard'>Dashboard</a>
-						<a href='/'>Sign Out</a>
+						<a href='/leaderboard' className='disable-link'>Leaderboard</a>
+						<a href='/chatroom' className='disable-link'>Chatroom</a>
+
 						{
 						//<a href='' className='icon' onclick={myFunction}>&#9776;</a>
 						}
