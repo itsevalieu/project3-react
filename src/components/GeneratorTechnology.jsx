87ee739f0407f5d4556c73ee7b42291504a6e249
Eva Lieu <itsevalieu@gmail.com>
2017-04-12 16:02:45 -0700

    Update GeneratorTechnology.jsx

--- src/components/GeneratorTechnology.jsx
+++ src/components/GeneratorTechnology.jsx
@@ -5,19 +5,24 @@ import GeneratorTechModal from './GeneratorTechModal.jsx';
 class GeneratorTechnology extends Component {
 	render() {
 		return(
-			<div id='techGenerator'>
-				<div className='container'>
-					<div className='generator-box'>
-						<h3>Teach Me Tech</h3>
-						<p>
-							Want to learn a new technology, but can't decide on which one?  Click below to teach yourself a randomly generated technolgy stack to use on your next project or revamp a previous code project.
-						</p>
-						<Modal header='Tech Stack Generator' trigger={<Button className='button generator-button'>GO!</Button>}>
-							<GeneratorTechModal />
-						</Modal>
-					</div>
-				</div>
-			</div>
+			<div>
+				<Col s={6}>
+					<Row>
+						<Col s={12}>
+						<h5 className='header light'>
+							I want to learn a new <b>technology</b>...
+						</h5>
+						</Col>
+					</Row>
+					<Row>
+						<Col s={12}>
+							<Modal trigger={<Button className='red btn btn-large' waves='light'>Tech Me Tech</Button>}>
+								<GeneratorTechModal />
+							</Modal>
+						</Col>
+					</Row>
+				</Col>
+			</div>	
 		);
 	}
 }
