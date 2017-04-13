4eccc49ea2c5ed101193aaf35d0cdc26db6bacfb
Eva Lieu <itsevalieu@gmail.com>
2017-04-13 15:35:02 -0700

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './src/index.js'
+    ],
+    output: {
+        path: path.join(__dirname, '/src/dist'),
+        filename: 'bundle.js',
+        publicPath: '/src/'
+    },
+    module: {
+        loaders: [
+            // js
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel-loader'],
+                include: path.join(__dirname, 'src')
+            },
+            {
+                test: /\.css$/,
+                loaders: ["style-loader", "css-loader"],
+                include: path.join(__dirname, "/src/public/assets/css")
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                loader: 'url-loader?name=/assets/images/[name].[ext]',
+                include: path.join(__dirname, "/src/public/assets/images")
+            }
+        ]
+    }
+};
